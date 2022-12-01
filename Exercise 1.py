3fa5cdc8953660f0daf093d7da07ba3d045139fa
tiwaribishal <114800074+tiwaribishal@users.noreply.github.com>
2022-12-01 23:22:26 +0530

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,8 @@
+# Question 1
+print("this is \\\ double backslash")
+# Question 2
+print("/\\/\\/\\/\\/\\")
+# Question 3
+print("he is    awesome")
+# Question 4
+print(" \\\" \\n \\t \\\' ")
---
+++ Lecture 40 - Assignment operatiors.py
@@ -0,0 +1,8 @@
+# Lecture 40 - Assignment operatiors
+name = "harsh"
+name = name + "it" #name  += "it"
+print(name)
+age=20 
+age += 1
+print(age)
+
